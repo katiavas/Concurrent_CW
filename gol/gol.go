2facd03f9b1643de17d3d1c7536ce2bfa0a656c9
katiavas <72811416+katiavas@users.noreply.github.com>
2020-11-16 21:19:59 +0000

    Update gol.go

--- gol/gol.go
+++ gol/gol.go
@@ -1,5 +1,7 @@
 package gol
 
+import "fmt"
+
 // Params provides the details of how to run the Game of Life and which image to load.
 type Params struct {
 	Turns       int
@@ -8,25 +10,106 @@ type Params struct {
 	ImageHeight int
 }
 
+/*func gameOfLife(p Params, initialWorld [][]byte) [][]byte {
+
+	world := initialWorld
+
+	for turn := 0; turn < p.Turns; turn++ {
+		world = calculateNextState(p, world)
+	}
+
+	return world
+}*/
+
+const alive = 255
+const dead = 0
+
+func mod(x, m int) int {
+	return (x + m) % m
+}
+
+func calculateNeighbours(p Params, x, y int, world [][]byte) int {
+	neighbours := 0
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			if i != 0 || j != 0 {
+				if world[mod(y+i, p.ImageHeight)][mod(x+j, p.ImageWidth)] == alive {
+					neighbours++
+				}
+			}
+		}
+	}
+	return neighbours
+}
+
+func calculateNextState(p Params, world [][]byte) [][]byte {
+	newWorld := make([][]byte, p.ImageHeight)
+	for i := range newWorld {
+		newWorld[i] = make([]byte, p.ImageWidth)
+	}
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			neighbours := calculateNeighbours(p, x, y, world)
+			if world[y][x] == alive {
+				if neighbours == 2 || neighbours == 3 {
+					newWorld[y][x] = alive
+				} else {
+					newWorld[y][x] = dead
+				}
+			} else {
+				if neighbours == 3 {
+					newWorld[y][x] = alive
+				} else {
+					newWorld[y][x] = dead
+				}
+			}
+		}
+	}
+	return newWorld
+}
+
+func calculateAliveCells(p Params, world [][]byte) []cell {
+	aliveCells := []cell{}
+
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			if world[y][x] == alive {
+				aliveCells = append(aliveCells, cell{x: x, y: y})
+			}
+		}
+	}
+
+	return aliveCells
+}
+
 // Run starts the processing of Game of Life. It should initialise channels and goroutines.
 func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
 
 	ioCommand := make(chan ioCommand)
 	ioIdle := make(chan bool)
+	//create a filename channel
+	ioFilename := make(chan string)
+	ioInput := make(chan uint8)
+	ioOutput := make(chan uint8)
 
 	distributorChannels := distributorChannels{
 		events,
 		ioCommand,
 		ioIdle,
 	}
-	go distributor(p, distributorChannels)
 
 	ioChannels := ioChannels{
 		command:  ioCommand,
 		idle:     ioIdle,
-		filename: nil,
-		output:   nil,
-		input:    nil,
+		filename: ioFilename,
+		output:   ioOutput,
+		input:    ioInput,
 	}
+	go distributor(p, distributorChannels)
+	//give a filename into the filename channel
+	//%dx%d bc images follow this specific format (e.g 16x16 ,512x512) and %d for base10
+	filename := fmt.Sprintf("%dx%d",p.ImageHeight,p.ImageWidth)
 	go startIo(p, ioChannels)
+	//send the filename down the ioFilename channel
+	ioFilename<-filename
 }
